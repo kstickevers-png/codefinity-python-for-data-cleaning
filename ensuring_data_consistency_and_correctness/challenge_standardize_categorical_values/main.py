c57d1874255531c21bb4d68ed1509abf6e448c70
kstickevers-png <>
2026-02-05 05:31:25 +0000

    Challenge: Standardize Categorical Values

---
+++ ensuring_data_consistency_and_correctness/challenge_standardize_categorical_values/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def standardize_column_case(df, column_name):
+    df = df.copy()
+    df[column_name] = df[column_name].str.lower()
+    return df
+
+data = {
+    "Response": ["Yes", "no", "YES", "No", "yes", "NO", "nO", "YeS"]
+}
+df = pd.DataFrame(data)
+result = standardize_column_case(df, "Response")
+print(result)
